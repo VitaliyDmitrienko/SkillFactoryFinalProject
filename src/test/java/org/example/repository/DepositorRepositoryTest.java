1caa121a762d41806134b92568c53570a200e8a4
ДмитриенкоВВ <vddjack2008@yandex.ru>
2024-01-23 16:11:54 +0300

    42th. Correct: /scr/test/resources/application.properties. Add DepositorRepositoryTest.getDepositorNullId + getDepositorOverExistId. Tests (all 3/3) pass OK.

--- src/test/java/org/example/repository/DepositorRepositoryTest.java
+++ src/test/java/org/example/repository/DepositorRepositoryTest.java
@@ -5,22 +5,42 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
 @DataJpaTest
+//@Sql(value = "classpath:init/user-data.sql", executionPhase = BEFORE_TEST_METHOD)
+//@Sql(value = "classpath:data.sql", executionPhase = BEFORE_TEST_METHOD)
 class DepositorRepositoryTest {
     @Autowired
     private DepositorRepository depositorRepository;
+    @Test
+    void getData_whenGetAll_countIsAll () {
+        List<Depositor> depositors = depositorRepository.findAll();
+//        Depositor depositorOne = depositors.get(1);
+//        System.out.println(depositors);
+//        System.out.println(depositorOne);
+        Assertions.assertEquals(4, depositors.size());
+    }
 
     @Test
-    void getData_whenGetAll_countIsThree () {
+    void getDepositorNullId () {
+        Optional<Depositor> depositorNull = depositorRepository.findById(0L);
+//        System.out.println(depositorNull);
+//        Assertions.assertNull(depositorNull);
+        Assertions.assertEquals(Optional.empty(),depositorNull);
+    }
+
+    @Test
+    void getDepositorOverExistId () {
         List<Depositor> depositors = depositorRepository.findAll();
-        Depositor depositorOne = depositors.get(1);
-        System.out.println(depositors);
-        System.out.println(depositorOne);
-        Assertions.assertEquals(3, depositors.size());
+        Optional<Depositor> depositorOver = depositorRepository.findById(depositors.size()+1L);
+        Assertions.assertEquals(Optional.empty(),depositorOver);
     }
 
 }
