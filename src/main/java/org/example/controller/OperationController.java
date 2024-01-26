d8ea3d5e16c80ba901f59427968d16e8e53e4a5b
VitaliyDmitrienko <vddjack2008@yandex.ru>
2024-01-26 16:50:32 +0300

    49th. Edit: OperationController.restGetOperationByDepositorIdAndOperationDateBetween change method signature to @PathVariable + @RequestParam to args. See URI & CURL query syntax in Swagger. Hide OperationController.restGetOperationByDepositorId (merge with *AndOperationDateBetween) to simplification.

--- src/main/java/org/example/controller/OperationController.java
+++ src/main/java/org/example/controller/OperationController.java
@@ -20,25 +20,26 @@ public class OperationController {
         this.operationService=operationService;
     }
 
-    @GetMapping("/getOperation/{depositorDonorId}")
-    ResponseEntity<?> restGetOperationByDepositorId(@PathVariable Long depositorDonorId) {
-        return new ResponseEntity<>((operationService.getOperationByDepositorId(depositorDonorId)), HttpStatus.OK);
-    }
+//    @GetMapping("/getOperation/{depositorDonorId}")
+//    ResponseEntity<?> restGetOperationByDepositorId(@PathVariable Long depositorDonorId) {
+//        return new ResponseEntity<>((operationService.getOperationByDepositorId(depositorDonorId)), HttpStatus.OK);
+//    }
 
 //    @GetMapping(value = {"/getOperationList/{depositorDonorId}/{beginDate}/{finishDate}",
 //            "/getOperationList/{depositorDonorId}/{beginDate}",
 //            "/getOperationList/{depositorDonorId}/{finishDate}",
 //            "/getOperationList/{depositorDonorId}"})
 //    @GetMapping ("/getOperationList/{depositorDonorId}")
-    @GetMapping ("/getOperationList")
+    @GetMapping ("/getOperationList/{depositorDonorId}")
     ResponseEntity<?> restGetOperationByDepositorIdAndOperationDateBetween
-        (@RequestParam (required = true) Long depositorDonorId,
+        (@PathVariable Long depositorDonorId,
         @RequestParam (required = false) LocalDateTime beginDate,
         @RequestParam (required = false) LocalDateTime finishDate) {
         if (beginDate != null && finishDate != null) {
             return new ResponseEntity<>((operationService.
                     getOperationByDepositorIdAndOperationDateBetween(depositorDonorId, beginDate, finishDate)),
-                HttpStatus.OK);}
+                HttpStatus.OK);
+    }
         else return new ResponseEntity<>((operationService.getOperationByDepositorId(depositorDonorId)),
                 HttpStatus.OK);
     }
---
+++ src/test/java/org/example/service/OperationServiceTest.java
@@ -0,0 +1,20 @@
+package org.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationServiceTest {
+
+    @Test
+    void storeOperation() {
+    }
+
+    @Test
+    void getOperationByDepositorId() {
+    }
+
+    @Test
+    void getOperationByDepositorIdAndOperationDateBetween() {
+    }
+}
