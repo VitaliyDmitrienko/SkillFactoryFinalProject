3f8409c0128f00954e78c41b08c511763fafe737
VitaliyDmitrienko <vddjack2008@yandex.ru>
2024-01-28 00:48:16 +0300

    52th. Trying to resolve conflict @ManyToOne & @OneToMany for Entities Depositor and Operation.

--- src/main/java/org/example/entity/Operation.java
+++ src/main/java/org/example/entity/Operation.java
@@ -17,9 +17,10 @@ public class Operation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column (name = "DEPOSITORDONORID")
+//    @Column (name = "DEPOSITORDONORID")
 //    @OneToMany
-//    @JoinColumn(name = "id")
+    @ManyToOne
+    @JoinColumn(name = "DepositorId")
     private Long depositorDonorId;
 
     @Column (name = "DEPOSITORACCEPTORID")
